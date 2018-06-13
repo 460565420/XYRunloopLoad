16c0f1d4142ca384aed9dd76c78e05080d893d2f
xieqilin <460565420@qq.com>
2018-06-13 15:25:34 +0800

    Initial Commit

---
+++ LoadBigImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadBigImageDemo
+//
+//  Created by xieqilin on 2018/6/13.
+//  Copyright © 2018年 xieqilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
